1f6cbcbe84173152efb4b3bb54368b2b8aea8d92
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2024-03-14 17:04:10 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -57,6 +57,7 @@ class Simulation:
         # States
         self.show_agent_debug_info = False
         self.show_agent_sensors = False
+        self.show_control_hotkeys = False
 
         # Timers
         self.timer_agent_updates = 0
@@ -151,8 +152,8 @@ class Simulation:
         """
         Draw all current game objects to a screen
         :param screen: pygame screen to draw the game objects on
-        :param delta_time_last_frame
-        :param show_debug_info
+        :param delta_time_last_frame: time since last frame draw. used to display fps in debug infos
+        :param show_debug_info: show debug values at top left of screen
         """
         timer_start = time.time()
 
@@ -207,67 +208,18 @@ class Simulation:
             screen.blit(text_surface, (140, 28))
 
         # Display hotkey infos
-        text_surface = self.debug_font.render("(T) Toggle Agent Info", True, blue)
-        screen.blit(text_surface, (2, self.size[1] - 18))
+        if self.show_control_hotkeys:
+            text_surface = self.debug_font.render("(T) Toggle Agent Info", True, blue)
+            screen.blit(text_surface, (2, self.size[1] - 18))
 
-        text_surface = self.debug_font.render("(R) Toggle Agent Sensors", True, blue)
-        screen.blit(text_surface, (2, self.size[1] - 34))
+            text_surface = self.debug_font.render("(R) Toggle Agent Sensors", True, blue)
+            screen.blit(text_surface, (2, self.size[1] - 34))
 
         self.timer_draw_frame = time.time() - timer_start
-
-    @staticmethod
-    def rotate_polygon(polygon, angle):
-        # Convert angle to radians
-        angle_rad = math.radians(angle)
-        # Define the rotation matrix
-        rotation_matrix = [
-            [math.cos(angle_rad), -math.sin(angle_rad)],
-            [math.sin(angle_rad), math.cos(angle_rad)]
-        ]
-        # Apply the rotation matrix to each point
-        rotated_polygon = []
-        for point in polygon:
-            rotated_point = [
-                point[0] * rotation_matrix[0][0] + point[1] * rotation_matrix[0][1],
-                point[0] * rotation_matrix[1][0] + point[1] * rotation_matrix[1][1]
-            ]
-            rotated_polygon.append(rotated_point)
-        return rotated_polygon
 
     def add_obstacle(self, position: (int, int), width: int, height: int):
         new_obstacle = Obstacle(position, width, height)
         self.obstacles.add(new_obstacle)
-
-    @staticmethod
-    def line_intersection(line1, line2, finite_line=True):
-        xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-        ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
-
-        def det(a, b):
-            return a[0] * b[1] - a[1] * b[0]
-
-        div = det(xdiff, ydiff)
-
-        # Lines do not intersect
-        if div == 0:
-            return None
-
-        d = (det(*line1), det(*line2))
-        x = round(det(d, xdiff) / div)
-        y = round(det(d, ydiff) / div)
-
-        if finite_line:
-            # Check if the intersection is within the bounds of line1
-            if not (min(line1[0][0], line1[1][0]) <= x <= max(line1[0][0], line1[1][0]) and
-                    min(line1[0][1], line1[1][1]) <= y <= max(line1[0][1], line1[1][1])):
-                return None
-
-            # Check if the intersection is within the bounds of line2
-            if not (min(line2[0][0], line2[1][0]) <= x <= max(line2[0][0], line2[1][0]) and
-                    min(line2[0][1], line2[1][1]) <= y <= max(line2[0][1], line2[1][1])):
-                return None
-
-        return x, y
 
     def calculate_collision_point(self, line, obstacle, multiple_collision_points=False):
         """
@@ -320,6 +272,55 @@ class Simulation:
         x2, y2 = point2
         return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
+    @staticmethod
+    def rotate_polygon(polygon, angle):
+        # Convert angle to radians
+        angle_rad = math.radians(angle)
+        # Define the rotation matrix
+        rotation_matrix = [
+            [math.cos(angle_rad), -math.sin(angle_rad)],
+            [math.sin(angle_rad), math.cos(angle_rad)]
+        ]
+        # Apply the rotation matrix to each point
+        rotated_polygon = []
+        for point in polygon:
+            rotated_point = [
+                point[0] * rotation_matrix[0][0] + point[1] * rotation_matrix[0][1],
+                point[0] * rotation_matrix[1][0] + point[1] * rotation_matrix[1][1]
+            ]
+            rotated_polygon.append(rotated_point)
+        return rotated_polygon
+
+    @staticmethod
+    def line_intersection(line1, line2, finite_line=True):
+        xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+        ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+        def det(a, b):
+            return a[0] * b[1] - a[1] * b[0]
+
+        div = det(xdiff, ydiff)
+
+        # Lines do not intersect
+        if div == 0:
+            return None
+
+        d = (det(*line1), det(*line2))
+        x = round(det(d, xdiff) / div)
+        y = round(det(d, ydiff) / div)
+
+        if finite_line:
+            # Check if the intersection is within the bounds of line1
+            if not (min(line1[0][0], line1[1][0]) <= x <= max(line1[0][0], line1[1][0]) and
+                    min(line1[0][1], line1[1][1]) <= y <= max(line1[0][1], line1[1][1])):
+                return None
+
+            # Check if the intersection is within the bounds of line2
+            if not (min(line2[0][0], line2[1][0]) <= x <= max(line2[0][0], line2[1][0]) and
+                    min(line2[0][1], line2[1][1]) <= y <= max(line2[0][1], line2[1][1])):
+                return None
+
+        return x, y
 
 if __name__ == '__main__':
     run_simulation(1280, 720, simulation_fps=60, number_of_agents=100, player_controlled_agent=True)
